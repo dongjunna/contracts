2bf007525c31d909a7f2c74974d0f88dbbd7039f
hyj <hyj@coinplug.com>
2023-08-16 15:39:24 +0900

    IChildChainManager: Fix mapToken method

--- contracts/child/ChildChainManager/IChildChainManager.sol
+++ contracts/child/ChildChainManager/IChildChainManager.sol
@@ -4,6 +4,6 @@ interface IChildChainManager {
     event TokenMapped(address indexed rootToken, address indexed childToken);
     event TokenUnmapped(address indexed rootToken, address indexed childToken);
 
-    function mapToken(address rootToken, address childToken) external;
+    function mapToken(address rootToken, address childToken, bool isErc721) external;
     function cleanMapToken(address rootToken, address childToken) external;
 }
